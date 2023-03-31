6354125ea9db607afe077e0463a31267993f83a0
CHAO FENG CHAN <peterjan.tw@gmail.com>
2023-03-31 12:49:05 +0800

    final version

--- chat.py
+++ chat.py
@@ -1,24 +1,38 @@
 
-def read_file(filename):
-	chats = []
-	with open(filename, 'r', encoding = 'utf-8-sig') as f:
-		for line in f:
-			chats.append(line.strip())
-	return chats
 
-def rewrite_file(filename, chats): # 一個function最好只做一件事，示範影片將convert和寫入拆成兩個function
-	with open(filename, 'w', encoding = 'utf-8-sig') as f: # new = []
-		for chat in chats:
-			if 'Allen' in chat: # if chat == 'Allen':
-				person = 'Allen'
-				continue
-			elif 'Tom' in chat:
-				person = 'Tom'
-				continue
-			f.write(person + ": " + chat + '\n') # new.append(person + ': ' + chat)
+def read_file(filename):
+	lines = []
+	with open(filename, 'r', encoding='utf-8-sig') as f:
+		for line in f:
+			lines.append(line.strip())
+	return lines
+
+
+def convert(lines):
+	new = []
+	person = None
+	for line in lines:
+		if line == 'Allen':
+			person = 'Allen'
+			continue
+		elif line == 'Tom':
+			person = 'Tom'
+			continue
+		if person:
+			new.append(person + ': ' + line)
+	return new
+
+
+def write_file(filename, lines):
+	with open(filename, 'w') as f:
+		for line in lines:
+			f.write(line + '\n')
+
 
 def main():
-	chats = read_file('input.txt')
-	rewrite_file('output.txt', chats)
+	lines = read_file('input.txt')
+	lines = convert(lines)
+	write_file('output.txt', lines)
+
 
 main()
